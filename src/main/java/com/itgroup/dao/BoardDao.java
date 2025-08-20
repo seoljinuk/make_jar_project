aaa5c7e53cf847388e275f861c427ea4949cc5d0
bluesky <blueksy@naver.com>
2025-08-20 18:03:53 +0900

    make jar project

--- src/main/java/com/itgroup/dao/BoardDao.java
+++ src/main/java/com/itgroup/dao/BoardDao.java
@@ -70,7 +70,36 @@ public class BoardDao extends SuperDao{
     }
 
     public List<Board> selectEvenData() {
+        // 전체 게시물을 최신 항목부터 조회하여 반환합니다.
+        List<Board> boardList = new ArrayList<Board>();
+
+        Connection conn = null ;
+        PreparedStatement pstmt = null ;
+        ResultSet rs = null ;
         String sql = "select * from boards where mod(no, 2) = 0 order by no desc " ;
-        return null ;
+
+        try{
+            conn = super.getConnection();
+            pstmt = conn.prepareStatement(sql) ;
+            rs = pstmt.executeQuery() ;
+
+            while(rs.next()){
+                Board bean = this.makeBean(rs);
+                boardList.add(bean) ;
+            }
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }finally {
+            try{
+                if(rs != null){rs.close();}
+                if(pstmt != null){pstmt.close();}
+                if(conn != null){conn.close();}
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+
+        return boardList ;
     }
 }
